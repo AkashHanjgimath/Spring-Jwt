659ff610f7e35e7f5621115c634a80feb216cc29
Akash Hanjgimath <akashhm10008@gmail.com>
2024-12-14 22:07:27 +0530

    User classes added

---
+++ src/main/java/com/springJwt/practice/model/MyUserDetailService.java
@@ -0,0 +1,47 @@
+package com.springJwt.practice.model;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private MyUserRepository myUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+      Optional<MyUser>user= myUserRepository.findByUsername(username);
+      if(user.isPresent())
+      {
+          var userobj=user.get();
+         return User.builder()
+                  .username(userobj.getUsername())
+                  .password(userobj.getPassword())
+                  .roles(getRoles(userobj))
+                  .build();
+      }
+      else{
+          throw new UsernameNotFoundException(username);
+
+      }
+
+    }
+
+    private String[] getRoles(MyUser user) {
+        if(user.getRole()==null)
+        {
+            return new String[]{"USER"};
+
+        }else {
+            return user.getRole().split(",");
+        }
+
+    }
+}
--- src/main/java/com/springJwt/practice/webtoken/SecurityConfiguration.java
+++ src/main/java/com/springJwt/practice/webtoken/SecurityConfiguration.java
@@ -1,9 +1,25 @@
+package com.springJwt.practice.webtoken;
+
+import com.springJwt.practice.model.MyUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 
 public class SecurityConfiguration {
+
+    @Autowired
+    private MyUserDetailService myUserDetailService;
+
     public DefaultSecurityFilterChain securityConfiguration(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
@@ -17,4 +33,35 @@ public class SecurityConfiguration {
                 .build();
     }
 
+    // interface  used to retrieve user details  from database,for authentication
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        return myUserDetailService;
+    }
+
+   //DaoAuthenticationProvider implementation of AuthenticationProvider
+   // that retrieves user details from a database (via a UserDetailsService),
+   // typically used for authenticating users against a database. It compares the username and password and performs authentication based on that.
+    @Bean
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider provoider=new DaoAuthenticationProvider();
+        provoider.setUserDetailsService(userDetailsService());
+        provoider.setPasswordEncoder(passwordEncoder());
+        return provoider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager()
+    {
+        return new ProviderManager(authenticationProvider());
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 }
